0993ba4e58ce420aed75b1a5f6989ae9765fe478
Gabri3lAlv3s788 <gabriel.barbosa11122006@gmail.com>
2024-10-15 02:10:51 -0300

    erros na imagem, não está ficando preto e branca

--- teste.py
+++ teste.py
@@ -1,10 +1,11 @@
 import cv2 as cv
+from PIL import Image
 
 metodo = [
     
     cv.THRESH_BINARY,
     cv.THRESH_BINARY_INV,
-    cv.THRESH_TRUNC,
+    cv.THRESH_TRUNC, 
     cv.THRESH_TOZERO,
     cv.THRESH_TOZERO_INV,
     
@@ -22,3 +23,15 @@ for metodos in metodo:
     _, tratada = cv.threshold(img_cinza, 127, 255, metodos or cv.THRESH_OTSU)
     cv.imwrite(f"2/imagem_tratada_{i}.png", tratada)
     
+
+img = Image.open("2/imagem_tratada_3.png")
+img = img.convert("P")
+img2 = Image.new("P", img.size, 255)
+
+for x in range(img.size[1]): # 1 = colunas da imagem
+    for y in range(img.size[0]): # 0 = linhas da imagem
+        cor = img.getpixel((y, x))
+        if cor < 115:
+            img2.putpixel((y, x), 0)
+        
+img2.save('2/img_final.png')
