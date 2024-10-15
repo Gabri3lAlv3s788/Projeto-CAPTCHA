735a105badbc36268df83d3eb83bdd9670785067
Gabri3lAlv3s788 <gabriel.barbosa11122006@gmail.com>
2024-10-15 14:20:58 -0300

    Tratamento certo das imagens

--- teste.py
+++ teste.py
@@ -2,13 +2,13 @@ import cv2 as cv
 from PIL import Image
 
 metodo = [
-    
+
     cv.THRESH_BINARY,
     cv.THRESH_BINARY_INV,
-    cv.THRESH_TRUNC, 
+    cv.THRESH_TRUNC,
     cv.THRESH_TOZERO,
     cv.THRESH_TOZERO_INV,
- 
+
 ]
 
 img = cv.imread("1/telanova0.png")
@@ -18,22 +18,21 @@ img_cinza = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 i = 0
 
 for metodos in metodo:
-    
+
     i += 1
     _, tratada = cv.threshold(img_cinza, 127, 255, metodos or cv.THRESH_OTSU)
     cv.imwrite(f"2/imagem_tratada_{i}.png", tratada)
-    
+
 
 img = Image.open("2/imagem_tratada_3.png")
 img = img.convert("P")
-img2 = Image.new ("P", img.size, color = (255, 255, 255))
+img2 = Image.new("P", img.size, color = (255, 255, 255))
 black = (0, 0, 0)
 
-for x in range(img.size[1]): # 1 = colunas da imagem
-    for y in range(img.size[0]): # 0 = linhas da imagem
+for x in range(img.size[1]):  # 1 = colunas da imagem
+    for y in range(img.size[0]):  # 0 = linhas da imagem
         cor = img.getpixel((y, x))
-        if cor < 115: 
+        if cor < 115:
             img2.putpixel((y, x), black)
-            print("AQUI")
-        
-img2.save('2/img_final.png')
+
+img2.save("2/img_final.png")
--- tratar_CAPTCHA.py
+++ tratar_CAPTCHA.py
@@ -1,3 +1,48 @@
 import cv2 as cv
+import os
+import glob
+from PIL import Image
 
 
+# Vai ver a pasta de origem e a pasta de destino
+def tratar_img(pasta_origem, pasta_destino="3"):
+    
+    arquivos = glob.glob(f"{pasta_origem}/*")
+    
+    for arquivo in arquivos:
+        
+        img = cv.imread(arquivo)
+
+        img_cinza = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+        _, tratada = cv.threshold(img_cinza, 127, 255, cv.THRESH_TRUNC or cv.THRESH_OTSU)
+        
+        nome = os.path.basename(arquivo)
+        
+        cv.imwrite(f"{pasta_destino}/{nome}", tratada)
+        
+        img = Image.open("2/imagem_tratada_3.png")
+        
+    arquivos = glob.glob(f"{pasta_destino}/*")
+    
+    for arquivo in arquivos:
+        
+        img = Image.open(arquivo)
+        
+        img = img.convert("P")
+        
+        img2 = Image.new("P", img.size, color = (255, 255, 255))
+        
+        black = (0, 0, 0)
+
+        for x in range(img.size[1]):  # 1 = colunas da imagem
+            for y in range(img.size[0]):  # 0 = linhas da imagem
+                cor = img.getpixel((y, x))
+                if cor < 115:
+                    img2.putpixel((y, x), black)
+        nome = os.path.basename(arquivo)
+        img2.save(f"{pasta_destino}/{nome}")
+
+
+if __name__ == "__main__":
+    tratar_img("1")
